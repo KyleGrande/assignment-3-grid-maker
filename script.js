21da5650074e093e90c29fbb1fd2d89e42a675a0
Kyle Grande <kyle.g.grande@gmail.com>
2023-03-17 18:04:32 -0500

    Added AddR and AddC

--- script.js
+++ script.js
@@ -2,15 +2,45 @@
 let numRows = 0;
 let numCols = 0;
 let colorSelected; 
+let table = document.getElementById("grid");
+let tableRow = document.getElementsByTagName("tr");
 
 // Add a row
 function addR() {
-    alert("Clicked Add Row"); // Replace this line with your code.
+
+    //checks if there are no rows if so there are also no columns
+    if (numRows === 0){
+        numCols = 0;
+    }
+
+    //add a row to the table and increment numRows
+    numRows += 1;
+    addRow = table.insertRow();
+
+    //then add a cell to the row for num of columns
+    let addCol;
+    for(let i=0; i<= numCols; i++){
+        addCol = addRow.insertCell();
+    }
 }
 
 // Add a column
 function addC() {
-    alert("Clicked Add Col"); // Replace this line with your code.
+    
+    //checks if there are no rows if so runs addR() to add a row
+    //and then returns to prevent adding a column
+    if (numRows === 0){
+        addR();
+        return;
+    }
+
+    //add a column to the table and increment numCols
+    numCols += 1;
+
+    //then add a cell to the row for num of rows
+    for (let i = 0; i < tableRow.length; i++) {
+        let newCell = tableRow[i].insertCell();
+    }
 }
 
 // Remove a row
